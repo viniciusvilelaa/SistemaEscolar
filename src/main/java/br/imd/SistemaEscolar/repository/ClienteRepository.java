1ada041e37348817c3a94961afa4bfdc68724b44
viniciusvilelaa <nino.bez@hotmail.com>
2024-10-09 06:01:07 -0300

    Criando os components, ambientes e configuration

---
+++ src/main/java/br/imd/SistemaEscolar/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package br.imd.SistemaEscolar.repository;
+import  br.imd.SistemaEscolar.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository {
+    public void salvar(Cliente cliente) {}
+
+}
